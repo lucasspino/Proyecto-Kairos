f4651d92eb5e37411fd81af2914e02c5872d4a03
lucasspino <lucaspino07@gmail.com>
2021-10-22 14:18:40 -0300

    CorreccionDesafioFinal

--- JavaScipt/macetas.js
+++ JavaScipt/macetas.js
@@ -1,9 +1,16 @@
+alert('Selecciona un producto. Cuando termines ingresa "Salir"');
+
+let prod = prompt('1)Kaidrá 2)Avavác 3)Polygono 4)Trigonikí Sfaíra 5)Strongyló 6)Trígono 7)Pyramída 8)Salir');
+
+const carrito = [];
+
 class Producto {
 
-    constructor (nombre, cantidad, precio){
-        this.nombre = nombre.toLowerCase();
-        this.cantidad = cantidad;
-        this.precio = precio;
+    constructor (nombre, cantidad, precio, id){
+        this.nombre = nombre.toLowerCase()
+        this.cantidad = cantidad
+        this.precio = precio
+        this.id = id
     }
 
     sumarIva (){
@@ -12,36 +19,59 @@ class Producto {
 
 }
 
-const carrito = [];
+const create = (articulo) => {
+    carrito.push(articulo);  
+}
+
+for (const producto of carrito){
+    producto.sumarIva()
+}
 
 const getAll = () => {
     return carrito;
 } 
 
-const create = (articulo) => {
-    carrito.push(articulo);
-}
-
-let producto1 = new Producto ("Kaidá", 1, 600);
-let producto3 = new Producto ("Avavác", 1, 500);
-let producto2 = new Producto ("Polygono", 1, 430);
-let producto4 = new Producto ("Trigonikí Sfaíra", 1, 470);
-let producto5 = new Producto ("Strongyló Polýgono", 1, 450);
-let producto6 = new Producto ("Trígono", 1, 450);
-let producto7 = new Producto ("Pyramída", 1, 450);
+let producto1 = new Producto ("Kaidá", 1, 600, "1");
+let producto3 = new Producto ("Avavác", 1, 500, "2");
+let producto2 = new Producto ("Polygono", 1, 430, "3");
+let producto4 = new Producto ("Trigonikí Sfaíra", 1, 470, "4");
+let producto5 = new Producto ("Strongyló Polýgono", 1, 450, "5");
+let producto6 = new Producto ("Trígono", 1, 450, "6");
+let producto7 = new Producto ("Pyramída", 1, 450, "7");
 
 
-create(producto1);
-create(producto2);
-create(producto3);
-create(producto4);
-create(producto5);
-create(producto6);
-create(producto7);
-
-
-for (const producto of carrito){
-    producto.sumarIva()
+while(prod != "8"){
+    switch (prod) {
+        case "1":
+            create(producto1);
+            break
+         case "2":
+            create(producto2);
+            break
+        case "3":
+            carrito.push(producto3);
+            break
+        case "4":
+            carrito.push(producto4);
+            break
+        case "5":
+            carrito.push(producto5);
+            break
+        case "6":
+            carrito.push(producto6);
+            break
+        case "7":
+            carrito.push(producto7);
+            break
+        case "8":
+            break;
+        default:
+            break;
+    }
+    prod = prompt('1)Kaidrá 2)Avavác 3)Polygono 4)Trigonikí Sfaíra 5)Strongyló 6)Trígono 7)Pyramída 8)Salir');
+    if (prod == "8"){
+        break
+    }
 }
 
 const oferta = carrito.filter(producto => producto.precio < 600);
@@ -58,7 +88,6 @@ let orden = carrito.sort(function(a, b){
     } 
 });
 
-
+console.log(oferta);
 
 console.log(getAll());
-console.log(oferta);
--- JavaScipt/velas.js
+++ JavaScipt/velas.js
@@ -1,9 +1,16 @@
+alert('Selecciona un producto. Cuando termines ingresa "Salir"');
+
+let prod = prompt('1)Kaidrá 2)Avavác 3)Polygono 4)Trigonikí Sfaíra 5)Strongyló 6)Trígono 7)Pyramída 8)Salir');
+
+const carrito = [];
+
 class Producto {
 
-    constructor (nombre, cantidad, precio){
-        this.nombre = nombre.toLowerCase();
-        this.cantidad = cantidad;
-        this.precio = precio;
+    constructor (nombre, cantidad, precio, id){
+        this.nombre = nombre.toLowerCase()
+        this.cantidad = cantidad
+        this.precio = precio
+        this.id = id
     }
 
     sumarIva (){
@@ -12,29 +19,44 @@ class Producto {
 
 }
 
-const carrito = [];
+const create = (articulo) => {
+    carrito.push(articulo);  
+}
+
+for (const producto of carrito){
+    producto.sumarIva()
+}
 
 const getAll = () => {
     return carrito;
 } 
 
-const create = (articulo) => {
-    carrito.push(articulo);
-}
 
-let producto1 = new Producto ("Amanecer", 1, 550);
-let producto2 = new Producto ("Hexagonal", 1, 480);
-let producto3 = new Producto ("Bombones", 1, 400);
+let producto1 = new Producto ("Amanecer", 1, 550, "1");
+let producto2 = new Producto ("Hexagonal", 1, 480, "2");
+let producto3 = new Producto ("Bombones", 1, 400, "3");
 
 
-create(producto1);
-create(producto2);
-create(producto3);
-
-
-
-for (const producto of carrito){
-    producto.sumarIva()
+while(prod != "8"){
+    switch (prod) {
+        case "1":
+            create(producto1);
+            break
+         case "2":
+            create(producto2);
+            break
+        case "3":
+            carrito.push(producto3);
+            break
+        case "8":
+            break;
+        default:
+            break;
+    }
+    prod = prompt('1)Kaidrá 2)Avavác 3)Polygono 4)Trigonikí Sfaíra 5)Strongyló 6)Trígono 7)Pyramída 8)Salir');
+    if (prod == "8"){
+        break
+    }
 }
 
 const oferta = carrito.filter(producto => producto.precio < 600);
@@ -51,7 +73,6 @@ let orden = carrito.sort(function(a, b){
     } 
 });
 
-
+console.log(oferta);
 
 console.log(getAll());
-console.log(oferta);
